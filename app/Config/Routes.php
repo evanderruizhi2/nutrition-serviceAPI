2645bd4ec86fcdbcfc5f58fd11424567b93556c2
evanderruizhi2 <ruizhixavier@gmail.com>
2026-01-03 23:54:01 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/ping', function () {
+    return json_encode(['status' => 'ok']);
+});
 $routes->group('api', function ($routes) {
     $routes->post('login', 'Api\AuthController::login');
 });
